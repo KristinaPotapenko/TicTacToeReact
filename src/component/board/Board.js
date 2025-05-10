989da8fe05e51238e70cfcaa14df2a13543ab93e
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-10 15:36:57 +0300

    Add state and click logic

--- src/component/board/Board.js
+++ src/component/board/Board.js
@@ -1,24 +1,71 @@
+import { useState } from "react";
 import { Square } from "../square/Square";
 import style from "./board.module.css";
 
 export const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (squareIndex) => {
+    if (squares[squareIndex]) return;
+
+    const copySquares = squares.slice();
+    copySquares[squareIndex] = "x";
+    setSquares(copySquares);
+  };
+
   return (
     <>
       <div className={style.board}>
         <div className={style.boardRow}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            squareIndex="0"
+            value={squares[0]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="1"
+            value={squares[1]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="2"
+            value={squares[2]}
+            onSquareClick={handleClick}
+          />
         </div>
         <div className={style.boardRow}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            squareIndex="3"
+            value={squares[3]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="4"
+            value={squares[4]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="5"
+            value={squares[5]}
+            onSquareClick={handleClick}
+          />
         </div>
         <div className={style.boardRow}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            squareIndex="6"
+            value={squares[6]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="7"
+            value={squares[7]}
+            onSquareClick={handleClick}
+          />
+          <Square
+            squareIndex="8"
+            value={squares[8]}
+            onSquareClick={handleClick}
+          />
         </div>
       </div>
     </>
--- src/component/square/Square.js
+++ src/component/square/Square.js
@@ -1,9 +1,17 @@
 import style from "./square.module.css";
 
-export const Square = () => {
+export const Square = ({ squareIndex, value, onSquareClick }) => {
+  let squareStyle = `${style.squareButton} `;
+
+  if (value === "x") {
+    squareStyle += style.squareX;
+  } else if (value === "o") {
+    squareStyle += style.squareO;
+  }
+
   return (
-    <div className={style.square}>
-      <button className={`${style.squareX} ${style.squareButton}`}></button>
+    <div className={style.square} onClick={() => onSquareClick(squareIndex)}>
+      <button className={squareStyle}></button>
     </div>
   );
 };
